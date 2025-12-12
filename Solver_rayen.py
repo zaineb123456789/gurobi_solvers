556ebad5d8f07218b1fa8ecbc6dc619bfb8d39fa
zaineb123456789 <zaineb.kchaou@gmail.com>
2025-12-12 14:03:51 +0100

    Update Solver_rayen.py

--- Solver_rayen.py
+++ Solver_rayen.py
@@ -16,7 +16,7 @@ class SolverThread(QThread):
     def run(self):
         try:
             model = Model("Production_PL")
-            model.setParam('OutputFlag', 0)
+            model.setParam('OutputFlag', 1)  # Enable output
 
             # Variables
             x = {p: model.addVar(vtype=GRB.CONTINUOUS, name=f"x_{p}", lb=0)
@@ -30,11 +30,15 @@ class SolverThread(QThread):
                 model.addConstr(sum(self.besoins[(p, r)] * x[p] for p in self.produits)
                                 <= self.disponibilite[r], name=f"R_{r}")
 
+            # Runs the Gurobi optimizer to solve the linear programming problem
             model.optimize()
 
             if model.status == GRB.OPTIMAL:
                 solution = {p: x[p].x for p in self.produits}
                 self.result_ready.emit(solution, model.objVal)
+
+                method_used = model.Params.Method
+                print(f"Method used: {method_used}")
             else:
                 self.result_ready.emit({}, 0.0)
 
